af65cc539402d391d7c322dd6e49f95fd4e879d1
jacpa42 <jacobenthoven@gmail.com>
2025-09-09 22:12:17 +0200

    fixed build.zig
    Turns out that it was just seg faulting when i tried to do shit

--- build.zig
+++ build.zig
@@ -1,71 +1,33 @@
 const std = @import("std");
 const sokol = @import("sokol");
+const Build = std.Build;
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const dep_sokol = b.dependency("sokol", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
-    const exe_mod = b.createModule(.{
+    const abalone_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+        .imports = &.{
+            .{ .name = "sokol", .module = dep_sokol.module("sokol") },
+        },
     });
 
     // Creates a binary
     const exe = b.addExecutable(.{
         .name = "abalone",
-        .root_module = exe_mod,
+        .root_module = abalone_mod,
     });
 
-    // sokol
-    const dep_sokol = b.dependency("sokol", .{
-        .target = target,
-        .optimize = optimize,
-    });
+    try compile_shaders(b, dep_sokol, exe);
+
     exe.root_module.addImport("sokol", dep_sokol.module("sokol"));
-
-    // Shader compilation
-    // extract shdc dependency from sokol dependency
-    const dep_shdc = dep_sokol.builder.dependency("shdc", .{});
-    const shader_dir = "src/shaders/";
-
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const shaders = find_shaders(&arena, shader_dir) catch @panic("Failed to find all shaders");
-    defer arena.deinit();
-
-    for (shaders.items) |shader_path_with_zig_ext| {
-        const len = shader_path_with_zig_ext.len;
-
-        const input = shader_path_with_zig_ext[0 .. len - ".zig".len];
-        const output = shader_path_with_zig_ext;
-        std.debug.print("Compiling shader {s} -> {s}\n", .{ input, output });
-
-        const shdc_step = sokol.shdc.createSourceFile(b, .{
-            .shdc_dep = dep_shdc,
-            .input = input,
-            .output = output,
-            .slang = .{
-                .glsl410 = false,
-                .glsl430 = true,
-
-                .glsl300es = true,
-                .glsl310es = true,
-
-                .hlsl4 = false,
-                .hlsl5 = true,
-
-                .metal_macos = true,
-                .metal_ios = true,
-                .metal_sim = true,
-                .wgsl = true,
-            },
-            .reflection = true,
-        }) catch @panic("");
-
-        exe.step.dependOn(shdc_step);
-    }
-
-    std.debug.print("Shaders compiled\n", .{});
 
     // qoi
     const qoi = b.dependency("qoi", .{ .target = target, .optimize = optimize });
@@ -95,7 +57,7 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the game :)");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{ .root_module = exe_mod });
+    const exe_unit_tests = b.addTest(.{ .root_module = abalone_mod });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
@@ -104,6 +66,51 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+}
+
+fn compile_shaders(b: *Build, dep_sokol: *Build.Dependency, abalone: *Build.Step.Compile) !void {
+    // Shader compilation
+    // extract shdc dependency from sokol dependency
+    const dep_shdc = dep_sokol.builder.dependency("shdc", .{});
+    const shader_dir = "src/shaders/";
+
+    var arena = std.heap.ArenaAllocator.init(b.allocator);
+    const shaders = find_shaders(&arena, shader_dir) catch @panic("Failed to find all shaders");
+    defer arena.deinit();
+
+    for (shaders.items) |shader_path_with_zig_ext| {
+        const len = shader_path_with_zig_ext.len;
+
+        const input = shader_path_with_zig_ext[0 .. len - ".zig".len];
+        const output = shader_path_with_zig_ext;
+        std.debug.print("Compiling shader {s} -> {s}\n", .{ input, output });
+
+        const shdc_step = sokol.shdc.createSourceFile(b, .{
+            .shdc_dep = dep_shdc,
+            .input = input,
+            .output = output,
+            .slang = .{
+                .glsl410 = false,
+                .glsl430 = true,
+
+                .glsl300es = true,
+                .glsl310es = false,
+
+                .hlsl4 = false,
+                .hlsl5 = true,
+
+                .wgsl = true,
+                .metal_macos = true,
+
+                .metal_ios = false,
+                .metal_sim = false,
+            },
+        }) catch @panic("");
+
+        abalone.step.dependOn(shdc_step);
+    }
+
+    std.debug.print("Shaders compiled\n", .{});
 }
 
 /// All the path strings and slices are allocated with the arena. Freeing the arena memory will clean up this bad boy.
